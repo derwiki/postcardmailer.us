dbb6b6b44c587643c61f0bb056999a4174a01b2a
Adam Derewecki <derewecki@gmail.com>
2018-04-06 17:17:43 -0700

    Add instagram and create_postcard

---
+++ lib/create_postcard.rb
@@ -0,0 +1,70 @@
+require 'rest_client'
+require 'json'
+
+DIRECT_MAIL_KEY = "OTc3YTJmYzQtNTIyMy00MWIxLTk0YmMtMzllYjg5ZGY1MGYwOmI5OTY5ZmYzLWE3YWUtNGIxOC1hMjZkLWFmMzk2ZGU0ZWJhMA=="
+
+# convert instagram.jpg -resize 1875x1350 instagram_1875x1350.jpg
+# convert sqinstagram.jpg -resize 1875x1350 -gravity center -extent 1875x1350 sqinstagram_1875x1350.jpg
+class CreatePostcard
+  attr_accessor :recipient, :front, :back, :dryrun
+
+  def initialize(recipient, front, back, dryrun: false)
+    self.recipient = recipient
+    self.front = front
+    self.back = back
+    self.dryrun = dryrun
+  end
+
+  def run
+    RestClient.post(
+      'https://print.directmailers.com/api/v1/postcard/',
+      JSON.generate(self.template),
+      self.headers
+    ).tap do |response|
+      p response
+      puts response
+    end
+  end
+
+  def headers
+    {
+      :content_type => 'application/json',
+      :authorization => "Basic #{DIRECT_MAIL_KEY}"
+    }
+  end
+
+  def template
+    {
+      "Description" => "#{Time.now} #{recipient[:name] }",
+      "Size" => "4.25x6",
+      "DryRun" => self.dryrun,
+      "WaitForRender" => true,
+      "To" => {
+        "Name" => recipient[:name],
+        "AddressLine1" => recipient[:address1],
+        "AddressLine2" => recipient[:address2],
+        "City" => recipient[:city],
+        "State" => recipient[:state],
+        "Zip" => recipient[:zip]
+      },
+      "From" => {
+        "Name" => "Adam Derewecki",
+        "AddressLine1" => "960 Wisconsin St",
+        "City" => "San Francisco",
+        "State" => "CA",
+        "Zip" => "94107"
+      },
+      "Front" => "#{self.front}",
+      "Back" => "#{self.back}",
+    }
+  end
+end
+
+create_postcard = CreatePostcard.new(
+  { name: 'Adam Derewecki', address1: '960 Wisconsin St', city: 'San Francisco', state: 'CA', zip: '94107' },
+  "https://i.imgur.com/Rm65gHT.jpg",
+  "https://i.imgur.com/Rm65gHT.jpg",
+  dryrun: true
+)
+p create_postcard.template
+create_postcard.run
---
+++ lib/instagram.rb
@@ -0,0 +1,15 @@
+require 'json'
+
+class Instagram
+
+  # returns 12 by default
+  def self.feed(username)
+    url = "https://www.instagram.com/#{username}/?__a=1"
+    feed_json = `curl --silent #{url}`
+    JSON[feed_json].dig("graphql", "user", "edge_owner_to_timeline_media", "edges").map do |edge|
+      edge.dig("node", "display_url")
+    end
+  end
+end
+
+puts Instagram.feed('derwiki')
